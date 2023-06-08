b29c17668a0fc68fce693e874da6ce07152a7b63
Nazzcodek <bellnas09@gmail.com>
2023-06-08 03:57:27 -0700

    task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+count = len(argv)- 1
+
+if __name__ == "__main__":
+    if count == 0:
+        print(f'{count} argument.')
+    elif count == 1:
+        print(f'{count} argument:')
+        print(f"1: {argv[1]}")
+    else:
+        print(f'{count} arguments:')
+        for i in range(1, count + 1):
+            print(f"{i}: {argv[i]}")
+
