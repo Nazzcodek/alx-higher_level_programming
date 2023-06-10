bdb5913d335b32b9661f04541041a3926e42943f
Nazzcodek <bellnas09@gmail.com>
2023-06-10 09:10:23 -0700

    advance task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <Python.h>
+
+/**
+ * print_list_list_info - print a pyhton list
+ *
+ * @p: list object to be printed
+ *
+ * Return: list
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	const char *type_name;
+	PyObject *element;
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+
+	for (Py_ssize_t i = 0; i < size; i++)
+	{
+		element = PyList_GetItem(p, i);
+		type_name = Py_TYPE(element)->tp_name;
+		printf("Element %ld: %s\n", i, type_name);
+	}
+}
