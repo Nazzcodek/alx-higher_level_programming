e484bdae04b4e3ab36b9370b2d6dbeedb183ac9a
Nazzcodek <bellnas09@gmail.com>
2023-06-14 10:47:11 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,11 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+
+    best_score = 0
+    name = ''
+    for k, v in a_dictionary.items():
+        if v > best_score:
+            best_score = v
+            name = k
+    return name
