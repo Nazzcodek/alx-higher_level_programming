bc998b7be44d95fa7559e6646a484b71e32fc6a0
Nazzcodek <bellnas09@gmail.com>
2023-06-14 06:46:58 -0700

    check for null

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return
     for i in sorted(a_dictionary.keys()):
         print("{} : {}".format(i, a_dictionary[i]))
