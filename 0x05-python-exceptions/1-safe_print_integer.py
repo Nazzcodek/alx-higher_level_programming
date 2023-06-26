1784577d9a0dfd4c3b6432391fc724b59f9de0fe
Nazzcodek <bellnas09@gmail.com>
2023-06-26 05:15:02 -0700

    task 1 and 2

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bash/pyhton3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        return False
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bash/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    try:
+        for i in my_list[:x]:
+            if isinstance(i, int):
+                print("{:d}".format(i), end='')
+                counter += 1
+        print()
+    except IndexError:
+        print("IndexError: list index out of range")
+    return counter
