29a9f08e62b7eacfd07a3af863e9fd7466437d87
Nazzcodek <bellnas09@gmail.com>
2023-06-27 09:18:16 -0700

    square define size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This is a Square class
+"""
+
+
+class Square:
+    """
+    Initializing the object class
+
+    size: the square size
+    """
+    def __init__(self, size):
+        self.__size = 10
