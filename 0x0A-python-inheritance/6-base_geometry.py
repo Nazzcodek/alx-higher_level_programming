54a29e3f89e78f23c39bacf1058bfbd5fcd02611
Nazzcodek <bellnas09@gmail.com>
2023-07-10 13:29:10 -0700

    base geometry area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+this is the module for the BaseGeomety class
+"""
+
+
+class BaseGeometry:
+    """ this is the base geomerty class """
+
+    """ public instance"""
+    def area(self):
+        raise Exception("area() is not implemented")
