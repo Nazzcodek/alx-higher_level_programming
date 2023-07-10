0b7301f5479a421f7b71baf2c37394d22ab95db3
Nazzcodek <bellnas09@gmail.com>
2023-07-10 12:23:08 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This is the module for the child class MyList
+"""
+
+
+class MyList(list):
+    """
+    This is the child class MyList
+
+    Inherit: 
+        from list module
+    """
+    def print_sorted(self):
+        """
+        This is the print_sorted function
+        """
+        print(sorted(self))
