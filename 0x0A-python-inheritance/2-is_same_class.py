e3e637353f03741619f9d76dff2796df5f346cf1
Nazzcodek <bellnas09@gmail.com>
2023-07-10 12:45:58 -0700

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+this is the module for the is_my_class function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    is_my _class function
+
+    Params:
+        obj: object to look for instance
+        a_class: class to lookup
+
+    Return:
+        boolean: True or False
+    """
+    return True if isinstance(obj, a_class) else False
