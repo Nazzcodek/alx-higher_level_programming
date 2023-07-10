9257ddecc3ff15b8b13154b54f68469e3ad9c759
Nazzcodek <bellnas09@gmail.com>
2023-07-10 15:49:04 -0700

    rectangle initialization

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,10 +6,17 @@ This is the module for Rectangle class
 
 
 class Rectangle(BaseGeometry):
-    """This is a child class to BaseGeometry"""
+    """
+    This is a child class
+
+    inheritance:
+        it inherit from BaseGeometry
+    """
 
     def __init__(self, width, height):
-        """ Initialize the instance
+        """ 
+            Initialize the instance
+
             Params:
                 width: width of the rectangle
                 height: height of the rectangle
