0a223fdaa03c8408263a8d2a8608801528696b7d
Nazzcodek <bellnas09@gmail.com>
2023-07-11 04:33:52 -0700

    from json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is the module for 4-from_json_string.py
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """ this function get string form json"""
+    return json.dumps(my_str)
