e62a2ef0a7cd2e05633f22b80d9dff9c0bf5ec84
Nazzcodek <bellnas09@gmail.com>
2023-07-11 04:53:24 -0700

    load from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This is the module for 6-load_from_json_file.py
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    This function load from json
+
+    Params:
+        filename: the file to load
+
+    Return:
+        loaded JSOn file
+    """
+    with open(filename, encoding='') as f:
+        content = f.read()
+        load = json.loads(content)
+        return load
