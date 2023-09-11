bff22f9bee96cf2b8dfd52e95234473beb790ff7
Nazzcodek <bellnas09@gmail.com>
2023-09-11 13:41:03 -0700

    12 and 13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2);
 if (args.length < 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    const numbers = args.map(Number).sort((a, b) => b - a);
-    console.log(numbers[1]);
+  const numbers = args.map(Number).sort((a, b) => b - a);
+  console.log(numbers[1]);
 }
