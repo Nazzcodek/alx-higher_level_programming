8e5905567ff294dcf54ed9254817493f3a442e42
Nazzcodek <bellnas09@gmail.com>
2023-09-11 11:35:46 -0700

    task 8 and 9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const arg = process.argv[2];
 const value = parseInt(process.argv[2], 10);
 if (arg && value) {
- for (let i = 0; i < arg; i++) {
-      console.log('X'.repeat(arg));
- }
+  for (let i = 0; i < arg; i++) {
+    console.log('X'.repeat(arg));
+  }
 } else {
   console.log('Not a number');
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,4 +4,3 @@ function add (a, b) {
   return (a + b);
 }
 console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
-
