36f06143c9ae4547e1449b644c0218e303437dc2
Nazzcodek <bellnas09@gmail.com>
2023-09-16 13:40:17 -0700

    model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is the model base module
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State object that inherits from Base
+        __tablename__: mysql table name
+        id: unique primary key
+        name: State name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(128), nullable=False)
