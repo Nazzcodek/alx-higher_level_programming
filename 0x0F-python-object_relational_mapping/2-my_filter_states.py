0e5a626ab54e12dfa46c81eab87d0684fec3bcda
Nazzcodek <bellnas09@gmail.com>
2023-09-17 07:43:09 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,6 +18,4 @@ if __name__ == "__main__":
                     FROM `states` \
                     WHERE `name` = '{}' \
                     ORDER BY `id`".format(sys.argv[4]))
-    states = [cursor.fetchall()]
-    for state in states:
-        print(state)
+    [print(state) for state in c.fetchall()]
