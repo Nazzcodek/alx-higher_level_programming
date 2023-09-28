2315f75bdb693700c06619847166c5e184e5fc24
Nazzcodek <bellnas09@gmail.com>
2023-09-28 13:00:11 -0700

    technical interview

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this is function defination file 6-peak.py
+"""
+def find_peak(list_of_integers):
+    size = len(list_of_integers)
+
+    if size == 0:
+        return None
+    if size == 1:
+        return list_of_integers[0]
+    mid = size // 2
+    peak = list_of_integers[mid]
+
+    if (mid == 0 or peak >= list_of_integers[mid - 1]) and \
+       (mid == size - 1 or peak >= list_of_integers[mid + 1]):
+        return peak
+
+    if peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
