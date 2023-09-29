4cc1f23b6ebe8c497409946b759f8b0a1849fe9b
Nazcodek <bellnas09@gmail.com>
2023-09-29 04:35:06 -0700

    correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     request = requests.get(url)
     print(f"Body response:\n"
-          f"    - type: {type(request.text)}\n"
-          f"    - content: {request.text}\n", end='')
+          f"\t- type: {type(request.text)}\n"
+          f"\t- content: {request.text}")
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {"email: " email}
+    data = {"email": email}
     request = requests.post(url, data=data)
     print(request.text)
