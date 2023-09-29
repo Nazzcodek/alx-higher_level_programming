20a53c0de7dfddb818253aadd29e7d11d9851bf0
Nazcodek <bellnas09@gmail.com>
2023-09-29 03:27:50 -0700

    task 4

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         request = requests.get(url)
+        request.raise_for_status()
         print(request.text)
     except requests.exceptions.RequestException as e:
-        if hasattr(e, 'code'):
-            print(f"Error code: {e.code}")
+        print(f"Error code: {e.response.status_code}")
